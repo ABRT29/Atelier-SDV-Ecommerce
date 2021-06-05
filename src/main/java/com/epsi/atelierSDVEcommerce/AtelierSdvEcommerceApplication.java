2d0622a544b0f14cad93cfd4180bda7dfd6490d0
abrx <aurelienbrtmail@gmail.com>
2021-06-05 09:30:31 +0200

    Java Sprint Init Project

---
+++ src/main/java/com/epsi/atelierSDVEcommerce/AtelierSdvEcommerceApplication.java
@@ -0,0 +1,13 @@
+package com.epsi.atelierSDVEcommerce;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AtelierSdvEcommerceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AtelierSdvEcommerceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/epsi/atelierSDVEcommerce/AtelierSdvEcommerceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.epsi.atelierSDVEcommerce;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AtelierSdvEcommerceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
